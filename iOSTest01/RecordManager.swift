226e2f609601976bab447c0e407a417f6c452cd8
mteera <i7676893@bournemouth.ac.uk>
2017-09-19 16:58:46 +0100

    Created RecordManager class
    Handles the Record object of CoreData

---
+++ iOSTest01/RecordManager.swift
@@ -0,0 +1,91 @@
+//
+//  RecordManager.swift
+//  iOSTest01
+//
+//  Created by Chace Teera on 19/09/2017.
+//  Copyright © 2017 Beep!. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class RecordManager {
+    
+    static let shared = RecordManager()
+    var record: Record!
+    var records = [Record]()
+    let context = PersistenceService.context
+    let request = NSFetchRequest<Record>(entityName: "Record")
+    
+    func addRecord(name: String, quantity: Int, date: Date, synched: Bool, uuid: String) {
+        
+        let record = Record(context:context)
+        record.name = name
+        record.quantity = Int16(quantity)
+        record.date = date as NSDate
+        record.synched = synched
+        record.uuid = uuid
+        PersistenceService.saveContext()
+    }
+    
+    func removeRecord(withUUID: String) {
+        
+        
+        do {
+            let searchResults = try context.fetch(request)
+            
+            for record in searchResults {
+                
+                if record.uuid == withUUID {
+                    
+                    // delete task
+                    context.delete(record)
+                }
+            }
+        } catch {
+            print("Error with request: \(error.localizedDescription)")
+        }
+        PersistenceService.saveContext()
+    }
+    
+    func editRecords(withUUID: String, name: String, quantity: Int, date: Date, synched: Bool) {
+        
+        do {
+            let searchResults = try context.fetch(request)
+            
+            for record in searchResults {
+                if record.uuid == withUUID {
+                    
+                    record.name = name
+                    record.quantity = Int16(quantity)
+                    record.synched = synched
+                    record.date = date as NSDate
+                    
+                }
+            }
+        } catch {
+            print("Error with request: \(error)")
+        }
+        PersistenceService.saveContext()
+    }
+    
+    //    func fetchRecord() -> [Record] {
+    //
+    //        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName:"Record")
+    //        fetchRequest.resultType = .dictionaryResultType
+    //
+    //        do {
+    //            records = try context.fetch(fetchRequest)
+    //            print(records)
+    //
+    //        } catch {
+    //            print("Error fetching data from CoreData")
+    //        }
+    
+    //        return records
+    //    }
+    //    
+    
+    
+}
